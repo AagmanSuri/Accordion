3174936262e49a56a14da6e9c12b4da035ddac9b
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-08-10 02:32:28 +0530

    data added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questinsData, setQuestionData] = useState(data);
+  return (
+    <>
+      <h2>accordion project setup</h2>
+      <SingleQuestion questinsData={questinsData} />
+    </>
+  );
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ questinsData }) => {
+  const [btnName, setBtnName] = useState("Plus");
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <h2>question component</h2>
+      {questinsData.map((question, index) => {
+        return (
+          <div key={index}>
+            <h3>{question.title}</h3>
+            <p>{question.info}</p>
+            <button>{btnName}</button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Question;
