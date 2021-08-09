d1beb3c48344c0a5a357b152c52dfdc23106c3d5
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-08-10 02:40:40 +0530

    btn

--- src/Question.js
+++ src/Question.js
@@ -3,6 +3,10 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ questinsData }) => {
   const [btnName, setBtnName] = useState("Plus");
   const [isOpen, setIsOpen] = useState(false);
+  const btnHandeler = (id) => {
+    console.log(id);
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
@@ -11,8 +15,8 @@ const Question = ({ questinsData }) => {
         return (
           <div key={index}>
             <h3>{question.title}</h3>
-            <p>{question.info}</p>
-            <button>{btnName}</button>
+            {isOpen || <p>{question.info}</p>}
+            <button onClick={() => btnHandeler(question.id)}>{btnName}</button>
           </div>
         );
       })}
